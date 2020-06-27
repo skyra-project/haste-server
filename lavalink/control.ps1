eaff67590fcd43bfa157ea5096247a86743ddde4
Jeroen Claassens <support@favware.tech>
2020-06-27 05:34:06 +0200

    💻: update login process for control files

--- lavalink/control.ps1
+++ lavalink/control.ps1
@@ -5,7 +5,11 @@ Function Step-Main {
 
     Process {
         switch ( $Command ) {
-            login { docker login }
+            login {
+                $Username = $( Read-Host "Please provide your GitHub username" )
+                $SecurePassword = $( Read-Host -AsSecureString "Please provide a GitHub token with access to publish packages" )
+                ConvertFrom-SecureString -SecureString $SecurePassword | docker login https://docker.pkg.github.com -u $Username --password-stdin
+            }
             build { docker build -t docker.pkg.github.com/skyra-project/docker-images/lavalink:latest . }
             run { docker container run -it docker.pkg.github.com/skyra-project/docker-images/lavalink:latest }
             deploy { docker push docker.pkg.github.com/skyra-project/docker-images/lavalink:latest }
