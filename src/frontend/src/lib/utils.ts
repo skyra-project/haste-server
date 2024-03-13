7d28ca4b5bb7afb6d54360c02f909b5c5c7597c6
Jeroen Claassens <support@favware.tech>
2024-03-13 23:18:45 +0100

    refactor: cleanup frontend code

--- src/frontend/src/lib/utils.ts
+++ src/frontend/src/lib/utils.ts
@@ -1,7 +1,3 @@
 export function selectElement<E extends Element = HTMLElement>(selector: string): E {
 	return document.querySelector<E>(selector)!;
 }
-
-export function selectElementAll(selector: string): NodeListOf<HTMLElement> {
-	return document.querySelectorAll<HTMLElement>(selector);
-}
