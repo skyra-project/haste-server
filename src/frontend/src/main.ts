6c9e3932760510e346057c44360532ebb9977b1a
Jeroen Claassens <support@favware.tech>
2024-03-13 23:18:39 +0100

    refactor: move index.css to the src dir

--- src/frontend/src/main.ts
+++ src/frontend/src/main.ts
@@ -1,18 +1,20 @@
 import '@pnotify/core/dist/Material.css';
 import '@pnotify/core/dist/PNotify.css';
 import 'highlight.js/styles/atom-one-dark-reasonable.css';
-import { Haste } from './lib/Haste';
-import './lib/hljsConfig';
+import './index.css';
+import './lib/hljsConfig.js';
+
+import { Haste } from './lib/Haste.js';
 
 const app = new Haste('hastebin');
 
-const handlePop = () => {
+const handlePop = async () => {
 	const path = window.location.pathname;
 	if (path === '/') {
 		app.newDocument();
 	} else {
-		app.loadDocument(path.substring(1, path.length));
+		await app.loadDocument(path.substring(1, path.length));
 	}
 };
 
-handlePop();
+void handlePop();
