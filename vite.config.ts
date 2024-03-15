fa3e5da5193be512a86da82139d371ca5c7bc62e
Jeroen Claassens <support@favware.tech>
2024-03-15 02:41:34 +0100

    fix: add autoprefixer for css vendor prefixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
+import autoprefixer from 'autoprefixer';
 import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	root: 'src/frontend',
+	css: {
+		postcss: {
+			plugins: [autoprefixer]
+		}
+	},
 	build: {
 		outDir: fileURLToPath(new URL('dist/frontend/', import.meta.url)),
 		chunkSizeWarningLimit: 1_000, // 1 MB
