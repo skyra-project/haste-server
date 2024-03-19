c07a08fba844bc8176d5a1fdf88937ecbfd34c91
Jeroen Claassens <support@favware.tech>
2024-03-19 01:33:27 +0100

    chore: clean console log

--- server/routes/documents/[id].get.ts
+++ server/routes/documents/[id].get.ts
@@ -4,10 +4,6 @@
 export default defineEventHandler(async (event) => {
 	const id = getRouterParam(event, 'id');
 
-	const config = useRuntimeConfig(event);
-
-	console.log(config.staticDocuments);
-
 	if (!id) {
 		throw createError({
 			statusCode: 400,
